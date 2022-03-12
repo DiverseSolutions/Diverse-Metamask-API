e9cced0929d4dd1cfd459c43f3d9d1707907f7dd
mnkhod <mnkhod.dev@gmail.com>
2022-03-12 17:25:26 +0800

    Added Mobile Responsiveness to Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,47 @@
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className="px-12">
-      <main className="h-screen flex justify-center items-center flex-col">
+    <div className="px-0 sm:px-12">
+      <main className="flex flex-col items-center justify-center h-screen">
 
-        <h1 className="text-7xl mb-12">
+        <h1 className="mb-12 text-2xl text-center sm:text-4xl lg:text-7xl">
           <a className="text-blue-600" href="https://www.dsolutions.mn/">Diverse</a> Metamask API SDK!
         </h1>
 
-        <div className="text-center my-6">
-          <p className="mb-5 text-4xl font-bold">Add Token Endpoint</p>
+        <div className="my-6 text-center">
+          <p className="mb-5 text-2xl font-bold sm:text-4xl">Add Token Endpoint</p>
 
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-900 px-3 text-3xl">{"/add-token?"}</code>
+            <code className="px-3 text-3xl text-gray-900 bg-gray-100">{"/add-token?"}</code>
           </p>
 
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-800 px-3">{"name=<TOKEN_NAME>"}</code>
+            <code className="px-3 text-gray-800 bg-gray-100">{"name=<TOKEN_NAME>"}</code>
           </p>
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-800 px-3">{"decimals=<TOKEN_DECIMAL>"}</code>
+            <code className="px-3 text-gray-800 bg-gray-100">{"decimals=<TOKEN_DECIMAL>"}</code>
           </p>
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-800 px-3">{"symbol=<TOKEN_SYMBOL>"}</code>
+            <code className="px-3 text-gray-800 bg-gray-100">{"symbol=<TOKEN_SYMBOL>"}</code>
           </p>
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-800 px-3">{"address=<TOKEN_ADDRESS>"}</code>
+            <code className="px-3 text-gray-800 bg-gray-100">{"address=<TOKEN_ADDRESS>"}</code>
           </p>
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-800 px-3">{"imgUrl=<TOKEN_IMG>"}</code>
+            <code className="px-3 text-gray-800 bg-gray-100">{"imgUrl=<TOKEN_IMG>"}</code>
           </p>
         </div>
 
-        <div className="text-center my-6">
-          <p className="mb-5 text-4xl font-bold">Get Ethereum Chains</p>
+        <div className="my-6 text-center">
+          <p className="mb-5 text-2xl font-bold sm:text-4xl">Get Ethereum Chains</p>
 
           <p className="mt-3">
-            <code className="bg-gray-100 text-gray-900 px-3 text-3xl">{"/api/chains"}</code>
+            <code className="px-3 text-3xl text-gray-900 bg-gray-100">{"/api/chains"}</code>
           </p>
         </div>
 
       </main>
 
-      <footer className="flex flex-grow py-8 border-t border-gray-300 justify-center items-center">
+      <footer className="flex items-center justify-center flex-grow py-8 border-t border-gray-300">
         <a
           href="https://www.dsolutions.mn/"
           target="_blank"
